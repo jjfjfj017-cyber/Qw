ddd3a8a23a0cf12d25ce6ec618cf211948be916b
wahhajb <164786203+wahhajb@users.noreply.github.com>
2024-12-05 20:23:34 +0300

    Update random-cat.js

--- plugins/random-cat.js
+++ plugins/random-cat.js
@@ -4,7 +4,7 @@ const handler = async (m, {conn, text}) => {
     const res = await fetch('https://api.thecatapi.com/v1/images/search');
     const img = await res.json();
     const caption = `
-*╮ ───═┅─┅─┅═─── ╭*      🇾🇪_ *الجنہـــــرال* \n* 😎. \n*╯ ───═┅─┅─┅═─── ╰*
+*╮ ───═┅─┅─┅═─── ╭*      _ *ستيف بوت* \n* 😎. \n*╯ ───═┅─┅─┅═─── ╰*
 `.trim();
     conn.sendFile(m.chat, img[0].url, 'cat.jpg', caption, m);
   } catch (e) {
